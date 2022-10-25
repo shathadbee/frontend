c5a956917bb095bf3b2649027fd21739c4813924
shathadbe <shathaaldbee@gmail.com>
2022-10-25 13:10:59 +0300

    18

---
+++ javascript ass18/iavascript.js
@@ -0,0 +1,8 @@
+
+
+
+  onload=function() {
+    document.querySelector("h1").style.backgroundColor = "lightblue";
+    document.getElementById("p").style.backgroundColor = "red";
+    document.getElementsByClassName("p")[0].style.backgroundColor = "blue";
+  }
